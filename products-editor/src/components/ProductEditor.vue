445ebec97fe776701d94234b97110480d8249029
Michael Sonnino <monnino@gmail.com>
2021-08-14 10:22:22 +0300

    commit before adding Veutify

--- products-editor/src/components/ProductEditor.vue
+++ products-editor/src/components/ProductEditor.vue
@@ -1,35 +1,43 @@
 <template>
     <form @submit="onSubmit">
-            <label for="name">Product: {{product.productName}}</label>
-            <br>
-            <label for="id">Product ID:</label>
-            <input type="text" id="id" disabled :value="product.id">
-            <br>
-            <label for="unitManufacturingCost">Unit Manufacturing Cost:</label>
-            <input type="number" id="unitManufacturingCost" v-model.number="currentProduct.cogs.unitManufacturingCost">
-            <br>
-            <label for="shipmentUnitCost">Shipment Unit Cost:</label>
-            <input type="number" id="shipmentUnitCost" v-model.number="currentProduct.cogs.shipmentUnitCost">
-            <br>
-            <label for="monthlyAdvertismentCost">Monthly Advertisment Cost:</label>
-            <input type="number" id="monthlyAdvertismentCost" v-model.number="currentProduct.cogs.monthlyAdvertismentCost">
-            <br>
-            <label for="manufacturingCountry">Manufacturing Country:</label>
-            <select id="manufacturingCountry" v-model="currentProduct.cogs.manufacturingCountry">
-                <option v-for="country in countriesList" :key="country.code" :value="country.code"> {{country.name}} </option>
-            </select>
-            <span>Selected: {{ currentProduct.cogs.manufacturingCountry }}</span>
-            <br>
-            <input class="button" type="submit" value="Save Changes">
+      <p>Product: {{currentProduct.productName}}</p>
+      <p>ID: {{currentProduct.id}} </p>
+      <BaseInput
+        v-model.number="currentProduct.cogs.unitManufacturingCost"
+        label="Unit Manufacturing Cost: "
+        type="Number"
+      />
+      <br>
+      <BaseInput
+        v-model.number="currentProduct.cogs.shipmentUnitCost"
+        label="Shipment Unit Cost: "
+        type="Number"
+      />
+      <br>
+      <BaseInput
+        v-model.number="currentProduct.cogs.monthlyAdvertismentCost"
+        label="Monthly Advertisment Cost: "
+        type="Number"
+      />
+      <br>
+      <label for="manufacturingCountry">Manufacturing Country:</label>
+      <select id="manufacturingCountry" v-model="currentProduct.cogs.manufacturingCountry">
+          <option v-for="country in countriesList" :key="country.code" :value="country.code"> {{country.name}} </option>
+      </select>
+      <br>
+      <input class="button" type="submit" value="Save Changes">
     </form>
 </template>
 
 <script>
 import CountriesList from './CountriesList.js'
 import ProductService from '../sevices/ProductService.js'
-
+import BaseInput from './BaseInput.vue'
 export default {
   name: 'ProductEditor',
+  components: {
+    BaseInput
+  },
   props: {
     product: {
       type: Object,
